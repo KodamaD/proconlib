0076a1961a29a7061925ae2e6cf1cb14aa29d8e2
KoD <20050904daikikodama@gmail.com>
2021-03-24 13:03:28 +0900

    add int_alias.cpp

---
+++ utility/int_alias.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+#include <cstddef>
+
+using i32 = std::int32_t;
+using u32 = std::uint32_t;
+using i64 = std::int64_t;
+using u64 = std::uint64_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+using isize = std::ptrdiff_t;
+using usize = std::size_t;
