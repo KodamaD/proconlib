f7edc35b2a6bf370ae92bbf8b0bca9322f9cf24f
KoD <20050904daikikodama@gmail.com>
2021-03-24 13:09:28 +0900

    add set_minmax.cpp

---
+++ utility/set_minmax.cpp
@@ -0,0 +1,19 @@
+#pragma once
+
+template <class T>
+bool setmin(T &lhs, const T &rhs) {
+    if (lhs > rhs) {
+        lhs = rhs;
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+bool setmax(T &lhs, const T &rhs) {
+    if (lhs < rhs) {
+        lhs = rhs;
+        return true;
+    }
+    return false;
+}
