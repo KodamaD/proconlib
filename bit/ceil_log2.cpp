cfe884f66c7117eb55649714c774e684f5ff7dff
KoD <20050904daikikodama@gmail.com>
2021-03-25 12:45:38 +0900

    add ceil_log.cpp, auto_realloc.cpp

---
+++ bit/ceil_log2.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "../utility/int_alias.cpp"
+
+constexpr u64 ceil_log2(const u64 x) {
+    u64 e = 0;
+    while (((u64) 1 << e) < x) ++e;
+    return e;
+}
---
+++ utility/auto_realloc.cpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "../utility/int_alias.cpp"
+#include "../bit/ceil_log2.cpp"
+#include <utility>
+#include <vector>
+
+template <class F>
+class AutoRealloc {
+    using R = typename decltype(std::declval<F>().operator()(std::declval<usize>()))::value_type;
+    F func;
+    std::vector<R> data;
+public:
+    explicit AutoRealloc(F &&func): func(std::forward<F>(func)), data() { }
+    explicit AutoRealloc(F &&func, const usize capacity): func(std::forward<F>(func)) { reserve(capacity); }
+    void reserve(const usize size) {
+        if (data.size() < size) {
+            const usize pow2 = ((usize) 1 << ceil_log2(size));
+            data = func(pow2);
+        }
+    }
+    R operator [] (const usize i) {
+        reserve(i + 1);
+        return data[i];
+    }
+};
