7e5b5f97c38ed0c78659a5b18bb5c95d2f7725e1
KoD <20050904daikikodama@gmail.com>
2021-03-31 11:13:07 +0900

    trivial fixes

--- test/example.test.cpp
+++ test/example.test.cpp
@@ -5,4 +5,5 @@ int main() {
     int a, b;
     std::cin >> a >> b;
     std::cout << a + b << '\n';
+    return 0;
 }
--- test/factorize.test.cpp
+++ test/factorize.test.cpp
@@ -13,4 +13,5 @@ int main() {
         }
     }
     std::cout << '\n';
+    return 0;
 }
